067279bde350e093dbe25a2de9a275803d5cb7a9
Emil Enevoldsen <github@eeapp.no>
2023-04-30 23:28:13 +0200

    Update HasExternalId.php

--- src/HasExternalId.php
+++ src/HasExternalId.php
@@ -2,7 +2,31 @@
 
 namespace Eeappdev\LaravelExternalId;
 
+use Illuminate\Database\Eloquent\Relations\MorphOne;
+
 trait HasExternalId
 {
+    public function external(): MorphOne
+    {
+        return $this->morphOne(ExternalId::class, 'externable');
+    }
 
+    public function addExternalId(string $externalId): ?string
+    {
+        $external = $this->external()->updateOrCreate([], [
+            'external_id' => $externalId,
+        ]);
+
+        return $external->external_id;
+    }
+
+    public function removeExternalId(): void
+    {
+        $this->external()->delete();
+    }
+
+    public function getExternalId(): ?string
+    {
+        return $this->external ? $this->external->external_id : null;
+    }
 }
